d22e6d6618cf4c5a36f2b33bd22c3843ff244217
yuki <yuki_svip@163.com>
2021-01-28 16:44:38 +0800

    Mybatis查询 1. ParamterType 一个查询参数、多个参数、POJO参数、Map参数、List参数、Set参数、Array参数 2. ResultType结果映射 映射为Map、映射为POJO 3. Mysql进行Null转换，使用JdbcType=null和 JdbcforNullType 4. ${} 和 #{}区别，模糊查询使用${}、mysql函数、bind标签

---
+++ mybatis-demos/01-simple-demo/01-simple-demo/src/main/java/com/yuki/OtherParamTest.java
@@ -0,0 +1,46 @@
+package com.yuki;
+
+import com.yuki.dao.GirlDao;
+import com.yuki.entity.Girl;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.junit.runners.MethodSorters;
+
+import java.util.List;
+import java.util.Map;
+
+/*
+* @desc - null转化、${}和#{}的区别
+*/
+@RunWith(JUnit4.class)
+@FixMethodOrder(value = MethodSorters.DEFAULT)
+public class OtherParamTest extends BaseTest{
+
+    /*
+    *  null -   1. mybaits用TypeHandler 把 null转换成jdbcType.OTHER
+    *           2. orcale驱动(orcale的jdbc实现)没实现jdbc.OTHER,mysql驱动实现了。
+    *           3. orcale驱动执行会报错。
+    * */
+    @Test
+    public void test1() {
+        GirlDao girlDao = sqlSession.getMapper(GirlDao.class);
+        Girl param = new Girl();
+        // param.setId(1);
+        param.setName("柳岩");
+        List<Girl> list = girlDao.queryNullCast(param);
+        System.out.println(list);
+    }
+
+
+    /*
+     *  resultType Pojo - ${} 和 #{} 的区别
+     * */
+    @Test
+    public void test2() {
+        GirlDao girlDao = sqlSession.getMapper(GirlDao.class);
+        List<Girl> list = girlDao.queryLikeName("周");
+        System.out.println(list);
+    }
+}
---
+++ mybatis-demos/01-simple-demo/01-simple-demo/src/main/java/com/yuki/ParamTest.java
@@ -0,0 +1,107 @@
+package com.yuki;
+
+import com.yuki.dao.GirlDao;
+import com.yuki.entity.Girl;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.junit.runners.MethodSorters;
+
+import java.util.*;
+
+/*
+*
+*  @desc 参数不同时候，mapper中如何使用。
+* */
+@RunWith(JUnit4.class)
+@FixMethodOrder(value = MethodSorters.DEFAULT) //声明顺序执行Junit
+public class ParamTest extends BaseTest{
+
+    /*
+    * one Param - #{value}
+    * */
+    @Test
+    public void test1() {
+        GirlDao girlDao = sqlSession.getMapper(GirlDao.class);
+        Girl girl = girlDao.queryById(1);
+        System.out.println(girl);
+    }
+
+    /**
+     * one Param - 使用arg0、arg1或者param1、param2
+     * */
+    @Test
+    public void test2() {
+        GirlDao girlDao = sqlSession.getMapper(GirlDao.class);
+        List<Girl> girl = girlDao.queryByNameAndPhone("柳岩","18209876577");
+        System.out.println(girl);
+    }
+
+    /**
+     * pojo Param -直接写属性名
+     * */
+    @Test
+    public void test3() {
+        GirlDao girlDao = sqlSession.getMapper(GirlDao.class);
+        Girl param = new Girl();
+        param.setId(1);
+        param.setName("柳岩");
+        param.setPhone("18209876577");
+        List<Girl> girl = girlDao.queryByPojo(param);
+        System.out.println(girl);
+    }
+
+    /**
+     * map param - 写对应的key
+     * */
+    @Test
+    public void test4() {
+        GirlDao girlDao = sqlSession.getMapper(GirlDao.class);
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("id",1);
+        map.put("name","柳岩");
+        List<Girl> girl = girlDao.queryByMap(map);
+        System.out.println(girl);
+    }
+
+    /**
+     * list param - 使用list查询
+     * */
+    @Test
+    public void test5() {
+        GirlDao girlDao = sqlSession.getMapper(GirlDao.class);
+        List<String> list = new ArrayList<String>();
+        list.add("柳岩");
+        list.add("18209876577");
+        List<Girl> girl = girlDao.queryByList(list);
+        System.out.println(girl);
+    }
+
+    /**
+     * list param - 使用Set查询
+     * */
+    @Test
+    public void test6() {
+        GirlDao girlDao = sqlSession.getMapper(GirlDao.class);
+        Set<String> set = new HashSet<String>();
+        set.add("柳岩");
+        set.add("苍老师");
+        set.add("Angelababy");
+        List<Girl> girl = girlDao.queryBySet(set);
+        System.out.println(girl);
+    }
+
+    /**
+     * array param - 写对应的idex
+     * */
+    @Test
+    public void test7() {
+        GirlDao girlDao = sqlSession.getMapper(GirlDao.class);
+        String[] arr = new String[2];
+        arr[0] = "柳岩";
+        arr[1] = "18209876577";
+        List<Girl> girl = girlDao.queryByArr(arr);
+        System.out.println(girl);
+    }
+}
