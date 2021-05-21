25a1c7e88f9fd00c880ee37b140c0bce592ca864
yuki <yuki_svip@163.com>
2021-05-21 17:21:19 +0800

    datasource-embeded

--- spring-boot-demos/datasource-embed/src/main/resources/sql/data.sql
+++ spring-boot-demos/datasource-embed/src/main/resources/sql/data.sql
@@ -2,4 +2,5 @@ INSERT INTO tbdemo (first_name, last_name, career)
 VALUES
 ('Aliko', 'Dangote', 'Billionaire Industrialist'),
 ('Bill', 'Gates', 'Billionaire Tech Entrepreneur'),
-('Folrunsho', 'Alakija', 'Billionaire Oil Magnate');
+('Folrunsho', 'Alakija', 'Billionaire Oil Magnate'),
+('BBB', 'BBB', 'BBB');
